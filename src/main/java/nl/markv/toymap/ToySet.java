c0ffee7e54e9baf0325b83402be7ab3db833d2df
Mark <mverleg.noreply@gmail.com>
2023-05-21 10:23:46 +0200

    Add size, isEmpty and (empty) iterator methods

--- src/main/java/nl/markv/toymap/ToySet.java
+++ src/main/java/nl/markv/toymap/ToySet.java
@@ -4,40 +4,64 @@ import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.concurrent.ThreadSafe;
 import java.util.Collection;
+import java.util.Iterator;
 
 @ThreadSafe
-public class ToySet<K> {
+public class ToySet<K> implements Iterable<K> {
 
     //private final HashMap<K, V> fakeTmp;  //TODO @mark: TEMPORARY! REMOVE THIS!
 
+    private final int elemCount;
     private final int bucketCnt;
     //TODO @mark: is this better than recomputing?
     private final int @NotNull [] hashes;
-    private final @NotNull K @NotNull [] keys;
+    /** Use Object because we cannot properly create a generic array */
+    private final @NotNull Object @NotNull [] keys;
     //@NotNull
     //private final V[] values;
     //TODO @mark: does it make sense to store keys and values in the same array (for cache locality)?
     //TODO @mark: does not work for primitives though, and needs casting
 
-    private ToySet(int[] hashes, @NotNull K @NotNull [] keys) {
+    private ToySet(int elemCount, int[] hashes, @NotNull K @NotNull [] keys) {
         this.bucketCnt = hashes.length;
-        assert keys.length == bucketCnt;
         this.hashes = hashes;
+        assert elemCount <= bucketCnt;
+        this.elemCount = elemCount;
+        assert keys.length == bucketCnt;
         this.keys = keys;
     }
 
     public <J> ToySet<J> from(Collection<@NotNull J> input) {
-
+        var n = determineInitialCapacity(input.size());
+        int[] hashes = new int[n];
+        Object[] keys = new Object[n];
+        //TODO @mark:
+        //noinspection unchecked
+        return (ToySet<J>) new ToySet<>(elemCount, hashes, keys);
     }
 
     public boolean contains(K lookupKey) {
         throw new IllegalStateException();  //TODO @mark:
     }
 
+    public boolean isEmpty() {
+        return this.elemCount == 0;
+    }
+
+    public int size() {
+        return this.elemCount;
+    }
+
+    @NotNull
+    @Override
+    public Iterator<K> iterator() {
+        throw new UnsupportedOperationException();  //TODO @mark:
+    }
+
     //TODO @mark: optimize this: balance size (larger wastes memory and also hurts cache locality)
     //TODO @mark: java one uses 75%, but has a special bucketing scheme, while https://www.youtube.com/watch?v=td0h7cv4cc0 says <50% for double hashing
-    private int determineCapacity(int elementCount) {
-        throw new IllegalStateException("find a prime close to 2*elementCount");
+    private static int determineInitialCapacity(int elementCount) {
+        return 2 * elementCount;
     }
 
     //TODO @mark: optimize this: there are some benefits to using something smart here, like DOS resistance
