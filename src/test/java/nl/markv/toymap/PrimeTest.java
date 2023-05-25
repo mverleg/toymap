c0ffeee23bd9d3786ed594f8b9e777f45d032571
Mark <mverleg.noreply@gmail.com>
2023-05-25 13:28:26 +0200

    Early exit when divisor sqrt(nr)

--- src/test/java/nl/markv/toymap/PrimeTest.java
+++ src/test/java/nl/markv/toymap/PrimeTest.java
@@ -5,22 +5,22 @@ import org.junit.jupiter.api.Test;
 public class PrimeTest {
 
     @Test
-    public void hardcodedPrimesMatchSieve() {
+    public void hardcodedPrimesMatchGenerated() {
         int primesFound = 1;
-        int[] computedPrimes = new int[2<<20];
+        int[] computedPrimes = new int[2<<24];
         computedPrimes[0] = 2;
-        outer: for (int i = 3; i < Integer.MAX_VALUE - 2; i += 2) {
+        outer: for (int nr = 3; nr < Integer.MAX_VALUE - 2; nr += 2) {
             //System.out.println("trying " + i);
             for (int divisor : computedPrimes) {
                 //System.out.println("try " + i + " / " + divisor);
-                if (divisor == 0) {
+                if (divisor == 0 || divisor * divisor > nr) {
                     break;
                 }
-                if (i % divisor == 0) {
+                if (nr % divisor == 0) {
                     continue outer;
                 }
             }
-            computedPrimes[primesFound++] = i;
+            computedPrimes[primesFound++] = nr;
         }
         System.out.println("found " + primesFound + " primes");
     }
