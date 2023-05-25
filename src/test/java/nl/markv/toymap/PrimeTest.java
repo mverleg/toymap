c0ffeed9da161c45009389c8b8e31996eaac73d8
Mark <mverleg.noreply@gmail.com>
2023-05-25 13:44:16 +0200

    Bad attempt at Sieve

--- src/test/java/nl/markv/toymap/PrimeTest.java
+++ src/test/java/nl/markv/toymap/PrimeTest.java
@@ -1,11 +1,15 @@
 package nl.markv.toymap;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import java.util.BitSet;
 
 public class PrimeTest {
 
+    @Disabled
     @Test
-    public void hardcodedPrimesMatchGenerated() {
+    public void hardcodedPrimesMatchGenerated0() {
         int primesFound = 1;
         int[] computedPrimes = new int[2<<24];
         computedPrimes[0] = 2;
@@ -23,5 +27,26 @@ public class PrimeTest {
             computedPrimes[primesFound++] = nr;
         }
         System.out.println("found " + primesFound + " primes");
+    }
+
+    @Test
+    public void hardcodedPrimesMatchGenerated() {
+        // Use Sieve but skip indices for numbers divisible by 2, 3 and 5, so iterate in steps of 30.
+        BitSet isComponsite = new BitSet();
+        int ix = 0;
+        isComponsite.set(ix++);  // 2
+        isComponsite.set(ix++);  // 3
+        isComponsite.set(ix++);  // 5
+        for (int realNr = 0; realNr < (2<<16); realNr += 30) {
+            isComponsite.set(ix) / realNr + 1;
+            isComponsite.set(ix) / realNr + 7;
+            isComponsite.set(ix) / realNr + 11;
+            isComponsite.set(ix) / realNr + 13;
+            isComponsite.set(ix) / realNr + 17;
+            isComponsite.set(ix) / realNr + 19;
+            isComponsite.set(ix) / realNr + 23;
+            isComponsite.set(ix) / realNr + 29;
+        }
+        System.out.println("found " + isComponsite.cardinality() + " primes");
     }
 }
