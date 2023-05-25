c0ffee794bf8989da72e46c9b522ec9cf2cfb914
Mark <mverleg.noreply@gmail.com>
2023-05-25 16:17:09 +0200

    Attempt to skip multiples of 2, 3 and 5 but too much extra complexity

--- src/test/java/nl/markv/toymap/PrimeTest.java
+++ src/test/java/nl/markv/toymap/PrimeTest.java
@@ -31,22 +31,27 @@ public class PrimeTest {
 
     @Test
     public void hardcodedPrimesMatchGenerated() {
-        // Use Sieve but skip indices for numbers divisible by 2, 3 and 5, so iterate in steps of 30.
+        // Use Sieve but skip indices for numbers divisible by 2, 3 and 5,
+        // so iterate in steps of 30 starting from 7.
         BitSet isComponsite = new BitSet();
         int ix = 0;
         isComponsite.set(ix++);  // 2
         isComponsite.set(ix++);  // 3
         isComponsite.set(ix++);  // 5
-        for (int realNr = 0; realNr < (2<<16); realNr += 30) {
-            isComponsite.set(ix) / realNr + 1;
-            isComponsite.set(ix) / realNr + 7;
-            isComponsite.set(ix) / realNr + 11;
-            isComponsite.set(ix) / realNr + 13;
-            isComponsite.set(ix) / realNr + 17;
-            isComponsite.set(ix) / realNr + 19;
-            isComponsite.set(ix) / realNr + 23;
-            isComponsite.set(ix) / realNr + 29;
-        }
+        int nextPrime = getNrForIndex(ix);
+        int max = (int) Math.ceil(Math.sqrt(Integer.MAX_VALUE));
+//      while (nextPrime < max) {}
+
+//        for (int realNr = 0; realNr < (2<<16); realNr += 30) {
+//            isComponsite.set(ix) / realNr + 1;
+//            isComponsite.set(ix) / realNr + 7;
+//            isComponsite.set(ix) / realNr + 11;
+//            isComponsite.set(ix) / realNr + 13;
+//            isComponsite.set(ix) / realNr + 17;
+//            isComponsite.set(ix) / realNr + 19;
+//            isComponsite.set(ix) / realNr + 23;
+//            isComponsite.set(ix) / realNr + 29;
+//        }
         System.out.println("found " + isComponsite.cardinality() + " primes");
     }
 }
